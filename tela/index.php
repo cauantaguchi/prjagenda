4135af2c672ac3289b6047089fc14938a2203886
cauantaguchi <cauantaguchi1337@gmail.com>
2019-02-18 21:46:46 -0300

    prjagenda

---
+++ tela/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE  HTML>
+
+<html lang ="pt-br">
+	<?php
+		include("default.php");
+	?>
+	<body>
+		<div class ="container">
+			<?php
+		include("menu.php");
+			?>
+		</div>
+	</body>
+
+
+
+</html>
